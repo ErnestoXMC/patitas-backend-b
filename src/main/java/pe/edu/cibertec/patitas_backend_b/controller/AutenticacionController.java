9cbbb2f9739396e2b5cd18ea513de9b4060e21fc
Ernesto Manrique <ernestopmc11@gmail.com>
2024-09-21 19:50:30 -0500

    Primer Avance

---
+++ src/main/java/pe/edu/cibertec/patitas_backend_b/controller/AutenticacionController.java
@@ -0,0 +1,23 @@
+package pe.edu.cibertec.patitas_backend_b.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pe.edu.cibertec.patitas_backend_b.dto.LoginRequestDTO;
+import pe.edu.cibertec.patitas_backend_b.dto.LoginResponseDTO;
+
+@RestController
+@RequestMapping("/autenticacion")
+public class AutenticacionController {
+//request es lo que recibo
+//Response es lo que mando, respondo
+
+    @PostMapping("/login")
+    public LoginResponseDTO login(@RequestBody LoginRequestDTO loginRequestDTO){//DTO => datos
+
+        return new LoginResponseDTO("00", "", "Julio Chavez", "juliochavez@gmail.com");
+    }
+
+
+}
